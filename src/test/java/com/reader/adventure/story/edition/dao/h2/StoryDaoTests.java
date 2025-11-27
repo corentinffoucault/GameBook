102ee06fd77c07bb87a6f4ef7400afb1ad069f8b
Corentin <corentinf@free.fr>
2025-11-27 12:07:32 +0100

    enrichissement des tests

--- src/test/java/com/reader/adventure/story/edition/dao/h2/StoryDaoTests.java
+++ src/test/java/com/reader/adventure/story/edition/dao/h2/StoryDaoTests.java
@@ -1,5 +1,9 @@
 package com.reader.adventure.story.edition.dao.h2;
 
+import com.reader.adventure.game.AttributeKey;
+import com.reader.adventure.game.ComparatorKey;
+import com.reader.adventure.story.edition.model.choice.ChoiceConditional;
+import com.reader.adventure.story.edition.model.condition.ConditionAttributes;
 import com.reader.adventure.story.edition.model.story.IStory;
 import com.reader.adventure.ui.player.FileLoader;
 import org.junit.jupiter.api.Test;
@@ -7,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class StoryDaoTests {
@@ -27,6 +31,14 @@ public class StoryDaoTests {
         assertEquals(4, storyDao.getNodeById("Noeud 1").getChoice().size());
         assertEquals("Vous avez trop envie de vous mettre au chaud à la taverne et manger une bonne soupe chaude avec un morceau de boudin. Vous dirigez donc vos pas vers le nord en empruntant ***", storyDao.getNodeById("Noeud 1").getChoice().get(0).getText());
         assertEquals("Noeud 1.1", storyDao.getNodeById("Noeud 1").getChoice().get(0).getNext());
+
+        assertInstanceOf(ChoiceConditional.class, storyDao.getNodeById("Noeud 1").getChoice().get(2));
+        ChoiceConditional choice = (ChoiceConditional) storyDao.getNodeById("Noeud 1").getChoice().get(2);
+        assertInstanceOf(ConditionAttributes.class, choice.getCondition());
+
+        ConditionAttributes condition = ((ConditionAttributes) choice.getCondition());
+        assertEquals(ComparatorKey.LT, condition.getComparator());
+        assertEquals(AttributeKey.AG,condition.getAttribute());
 
     }
 }
